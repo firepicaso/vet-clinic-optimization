b65df003a547346550126ef0bff3db3939abd3bc
unknown <albertkantwi@gmail.com>
2023-10-03 23:51:35 +0100

    Add visits table

--- schema.sql
+++ schema.sql
@@ -40,6 +40,14 @@ CREATE TABLE specializations(
   PRIMARY KEY(id)
 );
 
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
+);
+
 ALTER TABLE owners ADD COLUMN email VARCHAR(255);
 
 CREATE INDEX idx_visits_animal_id ON visits(animal_id);
